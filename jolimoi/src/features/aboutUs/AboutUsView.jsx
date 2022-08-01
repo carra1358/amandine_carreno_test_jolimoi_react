08fb6c44213c11994b9fcfd86d0ba846be0f06cf
amandine carreno <30933638+carra1358@users.noreply.github.com>
2022-08-01 18:44:01 +0200

    view completed
    - desktop views completed
    -mobile views completed

--- jolimoi/src/features/aboutUs/AboutUsView.jsx
+++ jolimoi/src/features/aboutUs/AboutUsView.jsx
@@ -1,8 +1,34 @@
-import "aboutUs.css"
-
-function aboutUsView() {
-
+import "./aboutUs.css"
+import propTypes from 'prop-types';
 
 
-    const cc = "teste";
+function AboutUsView({ img, msg }) {
+
+    return (
+        <div className="about_us">
+            <div className="about_us_img_container">
+
+                <img src={img} alt=" " className="about_us_img" />
+
+            </div>
+
+            <div className="about_us_msg_container">
+
+                <h1 className="about_us_msg">{msg}</h1>
+
+            </div>
+        </div>
+    )
 }
+
+AboutUsView.propTypes = {
+    img: propTypes.string,
+    msg: propTypes.any
+};
+
+
+
+
+export default AboutUsView;
+
+
--- jolimoi/src/features/search/SearchView.jsx
+++ jolimoi/src/features/search/SearchView.jsx
@@ -1,5 +1,31 @@
-import "search.css"
+import { useState } from "react"
+import "./search.css"
 
 function SearchView() {
 
+    const [input, setInput] = useState("")
+    // eslint-disable-next-line
+    const [results, setResult] = useState([])
+
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+            console.log(input)
+        }
+    }
+
+    return (
+        <div className="search">
+            <div className="search_bar">
+                <input id="input" type="text" onChange={(e) => setInput(e.target.value.replace(/ /g, '+'))} onKeyDown={handleKeyDown} className="search_input" />
+                <button type="button" onClick={() => console.log(input)} className="search_button">Search</button>
+            </div>
+            <ul className="search_result">
+                {
+                    results.length > 0 ? <li></li> : null
+                }
+            </ul>
+        </div>
+    )
 }
+
+export default SearchView
